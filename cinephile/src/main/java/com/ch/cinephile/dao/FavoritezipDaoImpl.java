ed91132564a8ab412717bcdb19992cd925ae594c
dnrlrl <leewwo2o@naver.com>
2020-09-02 00:01:16 +0900

    Add files via upload

--- cinephile/src/main/java/com/ch/cinephile/dao/FavoritezipDaoImpl.java
+++ cinephile/src/main/java/com/ch/cinephile/dao/FavoritezipDaoImpl.java
@@ -1,8 +1,12 @@
 package com.ch.cinephile.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.ch.cinephile.model.Favoritezip;
 
 @Repository
 public class FavoritezipDaoImpl implements FavoritezipDao{
@@ -15,6 +19,12 @@ public class FavoritezipDaoImpl implements FavoritezipDao{
 	@Override
 	public int selectZipnum(String c_id) {
 		return sst.selectOne("favoritezipns.selectZipnum", c_id);
+	}
+	public List<Favoritezip> searchHot() {
+		return sst.selectList("favoritezipns.searchHot");
+	}
+	public String getId(int zipnum) {
+		return sst.selectOne("favoritezipns.getId", zipnum);
 	}
 
 }
