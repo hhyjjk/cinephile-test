5a92ac28f808c83089997e38d21942483fe0d023
dnrlrl <leewwo2o@naver.com>
2020-09-07 18:22:04 +0900

    Add files via upload

--- cinephile/src/main/java/com/ch/cinephile/controller/ReserveController.java
+++ cinephile/src/main/java/com/ch/cinephile/controller/ReserveController.java
@@ -1,5 +1,9 @@
 package com.ch.cinephile.controller;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +29,7 @@ public class ReserveController {
 		return "movie/reserveSeat";
 	}
 	@RequestMapping("/payment")
-	public String payment(Movie movie, String numAdult, String numTeenager, String price, String numSeat,String local, String date,Model model,HttpSession session) {
+	public String payment(Movie movie, String numAdult, String numTeenager, String price, String numSeat,String local, String date,Model model,HttpSession session){
 		//private int book_cnt; 표갯수 사람수에 어차피 나옴
 		//private String book_pay; 결제 방법
 		//book.setBook_date(date); 결제 날짜 영화 상영일 나누기
@@ -36,6 +40,20 @@ public class ReserveController {
 		book.setBook_price(Integer.parseInt(price));
 		book.setC_id((String)session.getAttribute("c_id"));
 		book.setBook_loc(local);
+		
+		//날짜 변환
+		SimpleDateFormat beforeFormat = new SimpleDateFormat("yyyymmdd");
+		SimpleDateFormat afterFormat = new SimpleDateFormat("yyyy-mm-dd");
+        java.util.Date reverseDate = null;
+        try {
+        	reverseDate = beforeFormat.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        String transDate = afterFormat.format(reverseDate);
+        Date sqldate = Date.valueOf(transDate);
+		book.setBook_date(sqldate);
+		
 		String humantype="";
 		if(!numAdult.equals("0")) {
 			humantype+="성인 "+numAdult+"명 ";
@@ -47,5 +65,12 @@ public class ReserveController {
 		bs.insert(book);
 		model.addAttribute("book", book);
 		return "movie/payment";
+	}
+	@RequestMapping("paysuccess")
+	public String paysuccess(String msg,Model model) {
+		model.addAttribute("msg", msg);
+		//int result=Integer.parseInt(msg);
+		//model.addAttribute("result", result);
+		return "movie/paysuccess";
 	}
 }
