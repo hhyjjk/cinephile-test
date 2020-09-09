843760ca08f71c0264672ef254a2333454d78d09
dnrlrl <leewwo2o@naver.com>
2020-09-09 18:25:25 +0900

    Add files via upload

--- cinephile/src/main/java/com/ch/cinephile/dao/BookDao.java
+++ cinephile/src/main/java/com/ch/cinephile/dao/BookDao.java
@@ -1,9 +1,13 @@
 package com.ch.cinephile.dao;
 
+import java.util.List;
+
 import com.ch.cinephile.model.Book;
 
 public interface BookDao {
 
 	void insert(Book book);
+
+	List<Book> searchall(String c_id);
 
 }
--- cinephile/src/main/java/com/ch/cinephile/dao/BookDaoImpl.java
+++ cinephile/src/main/java/com/ch/cinephile/dao/BookDaoImpl.java
@@ -1,5 +1,7 @@
 package com.ch.cinephile.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,7 +13,9 @@ public class BookDaoImpl implements BookDao{
 	@Autowired
 	private SqlSessionTemplate sst;
 	public void insert(Book book) {
-		
 		sst.insert("bookns.insert", book);
+	}
+	public List<Book> searchall(String c_id) {
+		return sst.selectList("bookns.searchAll", c_id);
 	}
 }
