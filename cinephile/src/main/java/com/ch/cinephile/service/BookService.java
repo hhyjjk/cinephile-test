1e4cc5a7f9725138aee9749a193efaea2f0f97ed
dnrlrl <leewwo2o@naver.com>
2020-09-09 18:25:53 +0900

    Add files via upload

--- cinephile/src/main/java/com/ch/cinephile/service/BookService.java
+++ cinephile/src/main/java/com/ch/cinephile/service/BookService.java
@@ -1,9 +1,13 @@
 package com.ch.cinephile.service;
 
+import java.util.List;
+
 import com.ch.cinephile.model.Book;
 
 public interface BookService {
 
 	void insert(Book book);
+
+	List<Book> searchall(String c_id);
 
 }
--- cinephile/src/main/java/com/ch/cinephile/service/BookServiceImpl.java
+++ cinephile/src/main/java/com/ch/cinephile/service/BookServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ch.cinephile.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +13,10 @@ public class BookServiceImpl implements BookService{
 	@Autowired
 	private BookDao bd;
 
-	@Override
 	public void insert(Book book) {
 		bd.insert(book);
+	}
+	public List<Book> searchall(String c_id) {
+		return bd.searchall(c_id);
 	}
 }
